68a737a3a3e143ceb4573c6fb6112500ff17383f
brown919 <brown919@msu.edu>
2023-03-20 16:59:17 -0400

    stamina

---
+++ Assets/Scripts/StaminaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class StaminaController : MonoBehaviour
+{
+    public Slider staminaMeter;
+    public Stamina playerStamina;
+
+    private void Start()
+    {
+        playerStamina = GameObject.FindGameObjectWithTag("Player").GetComponent<Stamina>();
+        staminaMeter = GetComponent<Slider>();
+        staminaMeter.maxValue = playerStamina.maxStamina;
+        staminaMeter.value = playerStamina.maxStamina;
+    }
+
+    public void SetStamina(int st)
+    {
+        staminaMeter.value = st;
+    }
+}
