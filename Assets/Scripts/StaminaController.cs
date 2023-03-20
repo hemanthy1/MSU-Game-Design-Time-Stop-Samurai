b1a6a269a5d7f10525c9c886eaf07dfe84051cf0
brown919 <brown919@msu.edu>
2023-03-20 18:35:57 -0400

    meter fix

--- Assets/Scripts/StaminaController.cs
+++ Assets/Scripts/StaminaController.cs
@@ -2,27 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.InputSystem;
 
 public class StaminaController : MonoBehaviour
 {
     public Slider staminaMeter;
-    public Stamina playerStamina;
+    public bool timestop = false;
+// public Stamina playerStamina;
 
     private void Start()
     {
-        playerStamina = GameObject.FindGameObjectWithTag("Player").GetComponent<Stamina>();
-        staminaMeter = GetComponent<Slider>();
-        staminaMeter.maxValue = playerStamina.maxStamina;
-        staminaMeter.minValue = playerStamina.minStamina;
+// playerStamina = GameObject.FindGameObjectWithTag("Player").GetComponent<Stamina>();
+        //staminaMeter = GetComponent<Slider>();
+        staminaMeter.maxValue = 100;
+        staminaMeter.minValue = 0;
+        staminaMeter.value = staminaMeter.minValue;
     }
-
+    public void Update()
+    {
+        if (timestop == true)
+        {
+            staminaMeter.value += Time.deltaTime*10;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("space key was pressed");
+            //print("space key was pressed");
+            //print(staminaMeter.value);
+            //print(timestop);
+            if (timestop == false)
+                timestop = true;
+            else
+                timestop = false;
+        }
+        if (Input.GetKeyDown("v"))
+        {
+            staminaMeter.value -= 10;
+            if (staminaMeter.value < 0)
+                staminaMeter.value = 0;
+        }
+    }
     public void SetStamina(int st)
     {
         staminaMeter.value = st;
     }
-}
-public class Stamina : MonoBehaviour
-{
-    public int maxStamina = 100;
-    public int minStamina = 0;
 }
